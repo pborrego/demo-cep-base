67ed73734a4b383e149c7683b7ddea1256812624
Paul Borrego <paul.borrego@turner.com>
2017-09-15 18:00:17 -0400

    new: Bring in autocomplete and button components
    Removes all the old demo code and adds in the autocomplete and
    button components from react toolbox. Adds some small code for
    clearing and tracking the search term.

--- src/pages/demo/index.js
+++ src/pages/demo/index.js
@@ -1,69 +1,58 @@
 import React, { Component }  from 'react';
-import { Card, CardText, CardTitle } from '@coredev/cnn-react-material/build/card';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Autocomplete } from '@coredev/cnn-react-material/build/autocomplete';
+import { Button } from '@coredev/cnn-react-material/build/button';
 
-import CardList from '../../modules/card-list';
 import styles from './styles.css';
 
 
 
 class Demo extends Component {
 
-    static propTypes = {
-        match: PropTypes.shape({
-            params: PropTypes.shape({
-                title: PropTypes.string
-            })
-        })
-    };
+    static propTypes = {};
     static defaultProps = {};
 
     state = {
-        cards: 'ABCDEFGHIJLKMNOPQRSTUVWXYZ'.split('').map((letter) => {
-
-            return {
-                title: letter,
-                text: `This is card ${letter}.`
-            };
-        })
+        searchTerm: ''
     };
 
-    get featuredCard() {
+    constructor(props) {
 
-        const index = this.state.cards.findIndex(({ title }) => {
+        super(props);
 
-            return this.props.match.params.title === title.toLowerCase()
-        });
-
-        return (index > -1) ? index : 0;
+        this.updateSearchTerm = this.updateSearchTerm.bind(this);
+        this.clear = this.clear.bind(this);
     }
 
-    get strategy() {
+    updatedTerm(term) {
 
-        const { strategy } = this.props.match.params;
-        const possibleStrategies = ['normal', 'reorder', 'duplicate', 'truncated'];
+        return function nextState() {
 
-        return possibleStrategies.indexOf(strategy) > -1 ? strategy : 'normal';
+            return {
+                searchTerm: term
+            };
+        };
+    }
+
+    updateSearchTerm(term) {
+
+        this.setState(this.updatedTerm(term));
+    }
+
+    clear() {
+
+        this.setState(this.updatedTerm(''));
     }
 
     render() {
+
         return (
             <div className={styles.demo}>
                 <p>
-                    This is a demo showing react toolbox. In this demo
-                    you are able to deep link into four list of cards
-                    using the URL. There are three strategies that can
-                    be used for each card: <Link to="/demo/p/reorder">reorder</Link>, <Link to="/demo/p/duplicate">duplicate</Link> and <Link to="/demo/p/truncated">truncated</Link>.
+                    A demo for search type ahead!
                 </p>
-                <CardList featuredCardIndex={this.featuredCard} strategy={this.strategy}>
-                    {this.state.cards.map(({ title, text }, i) => (
-                        <Card key={`${title}${text}${i}`}>
-                            <CardTitle>{title}</CardTitle>
-                            <CardText>{text}</CardText>
-                        </Card>
-                    ))}
-                </CardList>
+                <Autocomplete hint="Search" multiple={false} value={this.state.searchTerm} onQueryChange={this.updateSearchTerm} />
+                <Button onClick={this.clear}>Clear</Button>
+                <Button>Search</Button>
             </div>
         );
     }
